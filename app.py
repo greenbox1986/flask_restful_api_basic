0e8f400866843239dba9dfeae7091e20bb9c36fa
ej.jamshidi <ej.jamshidi@gmail.com>
2021-11-29 01:21:34 +0800

    fix Procfile

--- app.py
+++ app.py
@@ -10,9 +10,7 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ('DATABASE_URL', 'sqlite:///data.db')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://nqhhlfuozdepub:c7287309c7bbd3992f7bf09a26a468a8c68e3a9a3604bf4591cbdab91444da5c@ec2-34-224-117-67.compute-1.amazonaws.com:5432/d2v59spqrh0hm0'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'Ehsan'
 api = Api(app)
@@ -23,9 +21,10 @@ api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(StoreList, '/stores')
 api.add_resource(ItemList, '/items')
+
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
     from db import db
     db.init_app(app)
-    app.run(port=5000, debug=True)
+    app.run()
