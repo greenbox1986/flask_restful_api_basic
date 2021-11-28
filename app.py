501e1dc91b7d1284dafdf9134dc7a0469beb00e7
ej.jamshidi <ej.jamshidi@gmail.com>
2021-11-29 00:54:52 +0800

    fix Procfile

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ from resources.store import Store, StoreList
 
 app = Flask(__name__)
 DATABASE_DEFAULT = 'postgres://nqhhlfuozdepub:c7287309c7bbd3992f7bf09a26a468a8c68e3a9a3604bf4591cbdab91444da5c@ec2-34-224-117-67.compute-1.amazonaws.com:5432/d2v59spqrh0hm0'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL',DATABASE_DEFAULT)#'sqlite:///data.db' ,
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL',DATABASE_DEFAULT)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', DATABASE_DEFAULT)#'sqlite:///data.db' ,
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 app.secret_key = 'Ehsan'
