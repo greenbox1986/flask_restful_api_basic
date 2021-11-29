e8d14c193f8e5036a2510be1cc29ad78dea5eea8
ej.jamshidi <ej.jamshidi@gmail.com>
2021-11-30 00:29:47 +0800

    jwt extended

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 
 from flask import Flask, render_template
 from flask_restful import Api
-from flask_jwt import JWT
+from flask_jwt_extended import JWTManager
 
 from security import authenticate, identity
 from resources.user import UserRegister, User
@@ -21,7 +21,7 @@ app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'Ehsan'
 api = Api(app)
 
-jwt = JWT(app, authenticate, identity) #/auth
+jwt = JWTManager(app) #not creating /auth
    
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>')
@@ -29,6 +29,7 @@ api.add_resource(StoreList, '/stores')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 api.add_resource(User, '/user/<int:user_id>')
+api.add_resource(UserLogin, '/login')
 
 if __name__ == '__main__':
     from db import db
