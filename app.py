61cc95f67bbfe0d627555abaf5b669562bea1674
ej.jamshidi <ej.jamshidi@gmail.com>
2021-11-29 18:52:52 +0800

    view/delete user

--- app.py
+++ app.py
@@ -27,8 +27,8 @@ api.add_resource(Store, '/store/<string:name>')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(StoreList, '/stores')
 api.add_resource(ItemList, '/items')
-api.add_resource(User, '/user/<int:user_id>')
 api.add_resource(UserRegister, '/register')
+api.add_resource(User, '/user/<int:user_id>')
 
 if __name__ == '__main__':
     from db import db
