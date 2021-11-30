c59ab9d4481ad2991f59c00648c6115f7d040c27
ej.jamshidi <ej.jamshidi@gmail.com>
2021-11-30 13:48:16 +0800

    admin access

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
 
+from db import db
 from security import authenticate, identity
-from resources.user import UserRegister
+from resources.user import UserRegister, User
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
@@ -27,8 +28,8 @@ api.add_resource(StoreList, '/stores')
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
+api.add_resource(User, '/user/<int:user_id>')
 
 if __name__ == '__main__':
-    from db import db
     db.init_app(app)
     app.run(port=5000, debug=True)
--- resources/user.py
+++ resources/user.py
@@ -21,7 +21,28 @@ class UserRegister(Resource):
         if UserModel.find_by_username(data['username']):
             return {"message": "A user with that username already exists"}, 400
 
-        user = UserModel(data['username'], data['password'])
+        user = UserModel(**data)
         user.save_to_db()
 
         return {"message": "User created successfully."}, 201
+
+
+class User(Resource):
+    """
+    This resource can be useful when testing our Flask app. We may not want to expose it to public users, but for the
+    sake of demonstration in this course, it can be useful when we are manipulating data regarding the users.
+    """
+    @classmethod
+    def get(cls, user_id: int):
+        user = UserModel.find_by_id(user_id)
+        if not user:
+            return {'message': 'User Not Found'}, 404
+        return user.json(), 200
+
+    @classmethod
+    def delete(cls, user_id: int):
+        user = UserModel.find_by_id(user_id)
+        if not user:
+            return {'message': 'User Not Found'}, 404
+        user.delete_from_db()
+        return {'message': 'User deleted.'}, 200
