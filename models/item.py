e4d7a7e5af62b50e360249805a4fee9e526183ea
ej.jamshidi <ej.jamshidi@gmail.com>
2021-11-30 13:25:22 +0800

    admin access

--- models/item.py
+++ models/item.py
@@ -2,7 +2,6 @@ from db import db
 
 
 class ItemModel(db.Model):
-
     __tablename__ = 'items'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -11,18 +10,27 @@ class ItemModel(db.Model):
 
     store_id = db.Column(db.Integer, db.ForeignKey('stores.id'))
     store = db.relationship('StoreModel')
-    
+
     def __init__(self, name, price, store_id):
         self.name = name
         self.price = price
         self.store_id = store_id
 
     def json(self):
-        return {'name': self.name, 'price': self.price}
-    
+        return {
+            'id': self.id,
+            'name': self.name,
+            'price': self.price,
+            'store_id': self.store_id
+        }
+
     @classmethod
     def find_by_name(cls, name):
-        return cls.query.filter_by(name=name).first() # SELECT * FROM items WHERE name=name LIMIT 1
+        return cls.query.filter_by(name=name).first()
+    
+    @classmethod
+    def find_all(cls):
+        return cls.query.all()
 
     def save_to_db(self):
         db.session.add(self)
