c44f4402eed5acaebea2b43d7a01b3393ff001a7
ej.jamshidi <ej.jamshidi@gmail.com>
2021-11-30 17:34:55 +0800

    fix bugs

--- app.py
+++ app.py
@@ -3,16 +3,15 @@ from flask_restful import Api
 from flask_jwt_extended import JWTManager
 
 from db import db
-from security import authenticate, identity
-from resources.user import UserRegister, User
+from resources.user import UserRegister, User, UserLogin
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://nqhhlfuozdepub:c7287309c7bbd3992f7bf09a26a468a8c68e3a9a3604bf4591cbdab91444da5c@ec2-34-224-117-67.compute-1.amazonaws.com:5432/d2v59spqrh0hm0'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#app.config['PROPAGATE_EXCEPTIONS'] = True
-app.config['JWT_SECRET_KEY'] = 'super-secret'
+app.config['PROPAGATE_EXCEPTIONS'] = True
+app.secret_key = 'MySecretKey'  # could do app.config['JWT_SECRET_KEY'] if we prefer
 api = Api(app)
 
 
@@ -21,7 +20,7 @@ def create_tables():
     db.create_all()
 
 
-jwt = JWTManager(app)  # /auth
+jwt = JWTManager(app)
 
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
@@ -33,4 +32,4 @@ api.add_resource(UserLogin, '/login')
 
 if __name__ == '__main__':
     db.init_app(app)
-    app.run()
+    app.run(port=5000, debug=True)
--- resources/user.py
+++ resources/user.py
@@ -1,22 +1,24 @@
 from flask_restful import Resource, reqparse
+from werkzeug.security import safe_str_cmp
+from flask_jwt_extended import create_access_token, create_refresh_token
 from models.user import UserModel
+
+_user_parser = reqparse.RequestParser()
+_user_parser.add_argument('username',
+                          type=str,
+                          required=True,
+                          help="This field cannot be blank."
+                          )
+_user_parser.add_argument('password',
+                          type=str,
+                          required=True,
+                          help="This field cannot be blank."
+                          )
 
 
 class UserRegister(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('username',
-                        type=str,
-                        required=True,
-                        help="This field cannot be blank."
-                        )
-    parser.add_argument('password',
-                        type=str,
-                        required=True,
-                        help="This field cannot be blank."
-                        )
-
     def post(self):
-        data = UserRegister.parser.parse_args()
+        data = _user_parser.parse_args()
 
         if UserModel.find_by_username(data['username']):
             return {"message": "A user with that username already exists"}, 400
@@ -25,7 +27,7 @@ class UserRegister(Resource):
         user.save_to_db()
 
         return {"message": "User created successfully."}, 201
-
+    
 
 class User(Resource):
     """
@@ -46,3 +48,22 @@ class User(Resource):
             return {'message': 'User Not Found'}, 404
         user.delete_from_db()
         return {'message': 'User deleted.'}, 200
+
+
+class UserLogin(Resource):
+    def post(self):
+        data = _user_parser.parse_args()
+
+        user = UserModel.find_by_username(data['username'])
+
+        # this is what the `authenticate()` function did in security.py
+        if user and safe_str_cmp(user.password, data['password']):
+            # identity= is what the identity() function did in security.py—now stored in the JWT
+            access_token = create_access_token(identity=user.id, fresh=True) 
+            refresh_token = create_refresh_token(user.id)
+            return {
+                'access_token': access_token,
+                'refresh_token': refresh_token
+            }, 200
+
+        return {"message": "Invalid Credentials!"}, 401
